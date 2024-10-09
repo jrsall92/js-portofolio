4e907c3e3e8880751b8950001b2c1d889e6d9b3b
John Salliaris <jrsall92@gmail.com>
2024-10-09 16:39:35 +0100

    removed unneeded theming

--- src/app/app.constant.ts
+++
@@ -1,4 +0,0 @@
-import {Theme} from './app.models';
-
-export const DEFAULT_THEME = Theme.Dark;
-export const LOCAL_STORAGE_THEME_KEY = 'ngx-markdown:theme';
--- src/app/project-page/project-page.component.ts
+++ src/app/project-page/project-page.component.ts
@@ -1,8 +1,6 @@
 import {Component} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
 import {MarkdownComponent} from 'ngx-markdown';
-import 'prismjs';
-import 'prismjs/components/prism-markdown.js';
 
 @Component({
   selector: 'app-project-page',
